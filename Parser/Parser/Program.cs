7687254b1c63732a37d3eb6396be9d1c6d9a4186
KotAleks <kotov2990@gmail.com>
2022-03-18 21:43:42 +0300

    Че то я тут натворил, хз. Макс разбирайся теперь

--- Parser/Parser/Program.cs
+++ Parser/Parser/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.IO;
+using System.Web;
+using System.Text.Json;
 
 namespace Parser
 {
     class Program
     {
+
+        public Program ()
+        {
+            
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"E:\MAKS\StringParser\Parser\Parser\Doc\string.txt";
+            
+            // делим полученный массив символов из файла path на строки по символу ;
+            string[] lines = Parser(path).ToString().Split(";");
+
+            string urlline = lines[0];
+
+            string url = HttpUtility.UrlDecode(urlline).Remove(0,26);
+
+             //  url = url.Remove(0,26);
+          
+            
+
+            //foreach (var s in stroki)
+            //{
+            //    string url = HttpUtility.UrlDecode(s);  // декодируем полученные строки
+            //  //  string jsonString = JsonSerializer.Serialize(url); // сериализируем урл в json
+            //}
+            
+
+            Console.WriteLine(url);
+            //  Console.WriteLine(Parser(path).ToString());
+            Console.ReadKey();
+
+        }
+
+        static string Parser (string path) 
+        {
+            string textfile = "";
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Файл отсутствует");
+            }
+            else 
+            {
+                string[] readText = File.ReadAllLines(path);
+                foreach (string s in readText)
+                {
+                    textfile += s;
+                }
+            }
+            return textfile;
         }
     }
 }
