71cd2a948780ba761ee23e7708772d196085148d
KotAleks <kotov2990@gmail.com>
2022-03-19 15:12:35 +0300

    Изменения по модели

---
+++ Parser/Parser/Models/Model_Price.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Models
+{
+    class Model_Price
+    {
+        public string name { get; set; }
+        public string Price { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
--- Parser/Parser/Program.cs
+++ Parser/Parser/Program.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Web;
 using System.Text.Json;
+using Parser.Models;
+using Newtonsoft.Json;
 
 namespace Parser
 {
@@ -24,18 +26,19 @@ namespace Parser
 
             string url = HttpUtility.UrlDecode(urlline).Remove(0,26);
 
-             //  url = url.Remove(0,26);
-          
-            
+            Model_Price ttt = JsonConvert.DeserializeObject<Model_Price>(url);
+
+          //  Model_Price model_Price = JsonSerializer.Deserialize<Model_Price>(url);
+
 
             //foreach (var s in stroki)
             //{
             //    string url = HttpUtility.UrlDecode(s);  // декодируем полученные строки
             //  //  string jsonString = JsonSerializer.Serialize(url); // сериализируем урл в json
             //}
-            
 
-            Console.WriteLine(url);
+            Console.WriteLine($"name: {ttt?.name}");
+        //    Console.WriteLine(url);
             //  Console.WriteLine(Parser(path).ToString());
             Console.ReadKey();
 
